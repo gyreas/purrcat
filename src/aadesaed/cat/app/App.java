7ccedc625b0b939b79921c86c4f4d06694f0c1b4
Saheed Adeleye <aadesaed@gmail.com>
2023-12-18 23:47:18 +0100

    Implement --number-nonblank

--- src/aadesaed/cat/app/App.java
+++ src/aadesaed/cat/app/App.java
@@ -19,9 +19,6 @@ public class App {
    private static String newline      = System.getProperty("line.separator");
 
    public static void main(String[] args) {
-      // if "--help" or "-h" (print_usage()) # prio 1 (highest)
-      // if "--version" or "-v" (print_version()) (2nd highest, except when it's the first opt) 
-      // 
       Args config;
       if (args.length == 0) config = Args.default_args();
       else                  config = Args.parse_args(args);
@@ -35,7 +32,6 @@ public class App {
          print_version(); 
          exit(0); 
       }
-
       if (config.files.size() == 0) System.out.println();
       else { 
          for (String path : config.files) {
@@ -44,11 +40,20 @@ public class App {
             try {
                BufferedReader file = Files.newBufferedReader(p);
                int i = 1;
+
                while (true) {
                   String line = file.readLine();
+
                   if (line == null) break;
-                  line = show_line_number(line, i);
-                  System.out.printf(line);
+
+                  if (config.display_line_numbers_nonblank && line.isBlank()) {
+                     System.out.println(); 
+                     continue;
+                  } else if (config.display_line_numbers) {
+                     line = show_line_number(line, i);
+                  }
+
+                  System.out.printf("%s\n", line);
                   i++;
                }
                // System.out.printf("Line: %s\n", line);
@@ -61,16 +66,18 @@ public class App {
 
    private static String show_line_number(String line, int lineno) {
       var line_with_no = new Formatter();
-      line_with_no.format("  %d    %s", lineno, line);
+      line_with_no.format("%6d\t%s", lineno, line);
       return line_with_no.toString();
    }
+
    private static void print_usage() {
       String usage = String.join(
          newline,
          app_meta(),
-         "-h, --help     display this help and exit",
-         "-n, --number   number all output lines",
-         "-V, --version  output version information and exit"
+         "-h, --help               display this help and exit",
+         "-n, --number             number all output lines",
+         "-b, --number-nonblank    number all nonempty output lines, overrides -n",
+         "-V, --version            output version information and exit"
       );
 
       System.out.printf("%s\n", usage);
--- src/aadesaed/cat/cmdline/Args.java
+++ src/aadesaed/cat/cmdline/Args.java
@@ -20,20 +20,20 @@ public class Args {
    public boolean             display_help;
    public boolean             display_version;
    public boolean             display_line_numbers;
-   public boolean             display_nonblank_lines;
+   public boolean             display_line_numbers_nonblank;
 
    public Args() {
-      this.files = new ArrayList<>();
+      files = new ArrayList<>();
    }
 
    /** Return an Args with default values */
    public static Args default_args() {
-      var config                       = new Args();
-      config.display_help              = false;
-      config.display_line_numbers      = false;
-      config.display_version           = true; // indicates that the app works, just no input file
-      config.display_nonblank_lines    = true;
-      config.files                     = new ArrayList<>();
+      var config                             = new Args();
+      config.display_help                    = false;
+      config.display_line_numbers            = false;
+      config.display_version                 = true; // indicates that the app works, just no input file
+      config.display_line_numbers_nonblank   = true;
+      config.files                           = new ArrayList<>();
       return config;
    }
 
@@ -46,23 +46,25 @@ public class Args {
       }
 
       Args config = new Args();
-      ArrayList<String> a = new ArrayList<>(Arrays.asList(argv));
+      ArrayList<String> args = new ArrayList<>(Arrays.asList(argv));
 
       int i = 0; // always get the first element since we're consuming
-      while (a.size() != 0) {
-         String arg = a.get(i);
+      while (args.size() != 0) {
+         String arg = args.get(i);
          if (arg.equals("-h") || arg.equals("--help")) {
             config.display_help = true;
-            a.remove(i);
+            args.remove(i);
          } else if (arg.equals("-V") || arg.equals("--version")) {
             config.display_version = true;
-            a.remove(i);
+            args.remove(i);
          } else if (arg.equals("-n") || arg.equals("--number")) {
             config.display_line_numbers = true;
-            System.out.println("Displaying line numbers.");
-            a.remove(i);
+            args.remove(i);
+         } else if (arg.equals("-b") || arg.equals("--number-nonblank")) {
+            config.display_line_numbers_nonblank = true;
+            args.remove(i);
          } else if (arg.equals("--")) {
-            config.files = new ArrayList<>(a.subList(i + 1, a.size()));
+            config.files = new ArrayList<>(args.subList(i + 1, args.size()));
             return config;
          }
          else if (arg.startsWith("-")) {
@@ -71,7 +73,7 @@ public class Args {
          } else {
             // TODO: `write` to output stream, no `print`
             config.files.add(arg);
-            a.remove(i);
+            args.remove(i);
          }
       }
 
