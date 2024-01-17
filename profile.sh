55b865348b44ab7eaca26763a408c9536cb279fa
Saheed Adeleye <aadesaed@gmail.com>
2024-01-17 22:39:17 +0100

    Leave only VisualVM options.

--- profile.sh
+++ profile.sh
@@ -1,10 +1,7 @@
-PROJECTDIR="$HOME/learnings/java/purrcat"
-CP="$PROJECTDIR/target/classes"
+CP="target/classes"
 
 java \
   -ea \
   -cp $CP \
-  -agentpath:/home/saed/Downloads/YourKit-JavaProfiler-2023.9-b103-x64/YourKit-JavaProfiler-2023.9/bin/linux-x86-64/libyjpagent.so=disablestacktelemetry,exceptions=disable,delay=10000 \
-  -agentpath:/home/saed/jprofiler14/bin/linux-x64/libjprofilerti.so=port=8849\
   -agentpath:/home/saed/Downloads/visualvm_217/visualvm_217/visualvm/lib/deployed/jdk16/linux-amd64/libprofilerinterface.so=/home/saed/Downloads/visualvm_217/visualvm_217/visualvm/lib,5140 \
   aadesaed.cat.app.App "$@"
