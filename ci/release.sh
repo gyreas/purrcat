888a9e8ad7bcb45d9ae9e69011be1667ba60e47c
Saheed Adeleye <aadesaed@gmail.com>
2025-02-09 10:25:01 +0100

    fix: CI

--- ci/release.sh
+++ ci/release.sh
@@ -1,9 +1,6 @@
 CI=1 make package
 VERSION="$(git tag --sort -taggerdate | head -n1 | cut -c 2-)"
 
-ls $(pwd)/target
-echo "$version"
-
 gh release create "v$VERSION"        \
    --generate-notes                  \
   $(pwd)/target/purrcat-$VERSION.jar \
