95174d9ae526ec6bd26c8b16079b148eb1842bbf
Saheed Adeleye <aadesaed@gmail.com>
2025-02-09 11:29:57 +0100

    fix CI

--- ci/release.sh
+++ ci/release.sh
@@ -1,19 +1,15 @@
 #!/usr/bin/bash
 
-# set -xue
-
 CI=1 make package
 
-git log --oneline
-git show --minimal
- 
-gh release list
+PROPS="$(find src -regex .*\.properties)"
+VERSION="$(grep -E '[0-9]+\.[0-9]+\.[0-9]+(-SNAPSHOT)?' "$PROPS" | cut -d: -f2 | cut -c 2-)"
 
-VERSION="$(git tag --sort -taggerdate | head -n1 | cut -c 2-)"
+# TODO: reconsider this
+# VERSIONREL="$(echo $VERSION | cut -d- -f1)"
+# SNAPSHOT="$(echo $VERSION | cut -d- -f2)"
 
-echo $VERSION
-
-gh release create "v$VERSION" \
-   --generate-notes           \
-  target/purrcat-$VERSION.jar \
-  target/purrcat
+gh release create "purrcat-v$VERSION" \
+   --generate-notes                   \
+   target/purrcat-$VERSION*.jar       \
+   target/purrcat
